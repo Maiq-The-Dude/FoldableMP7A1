f2b080947ec208c580f7c8ad71a47b9fbc464147
M'aiq <m.janik1031@gmail.com>
2021-04-19 12:09:05 -0500

    Add proj files

---
+++ FoldableMP7A1/src/Hooks.cs
@@ -0,0 +1,54 @@
+﻿using FistVR;
+using UnityEngine;
+
+namespace FoldableMP7A1
+{
+	public class Hooks
+	{
+		public Hooks()
+		{
+			Hook();
+		}
+
+		public void Dispose()
+		{
+			Unhook();
+		}
+
+		private void Hook()
+		{
+			On.FistVR.ClosedBoltWeapon.Awake += ClosedBoltWeapon_Awake;
+		}
+
+		private void ClosedBoltWeapon_Awake(On.FistVR.ClosedBoltWeapon.orig_Awake orig, FistVR.ClosedBoltWeapon self)
+		{
+			orig(self);
+
+			if (self.ObjectWrapper.ItemID == "Mp7a1")
+			{
+				AddFoldingHandle(self);
+			}
+		}
+
+		private void AddFoldingHandle(ClosedBoltWeapon weapon)
+		{
+			var geo = weapon.transform.Find("Geo");
+			var flipper = geo.Find("FlipSightTrigger_Front");
+			var handle = geo.Find("Handle");
+
+			var handleFlipper = GameObject.Instantiate(flipper, geo);
+			handleFlipper.transform.localPosition = new Vector3(0, 0, 0.1f);
+
+			var hflipper = handleFlipper.gameObject.GetComponent<AR15HandleSightFlipper>();
+			hflipper.name = "HandleFlipper";
+			hflipper.Flipsight = handle;
+
+			weapon.Foregrip.transform.parent = handle;
+		}
+
+		private void Unhook()
+		{
+			On.FistVR.ClosedBoltWeapon.Awake -= ClosedBoltWeapon_Awake;
+		}
+	}
+}
---
+++ FoldableMP7A1/src/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using Deli.Setup;
+
+namespace FoldableMP7A1
+{
+	public class FoldableMP7A1 : DeliBehaviour
+	{
+		private readonly Hooks _hooks;
+
+		public FoldableMP7A1()
+		{
+			_hooks = new Hooks();
+		}
+
+		private void OnDestroy()
+		{
+			_hooks?.Dispose();
+		}
+	}
+}
